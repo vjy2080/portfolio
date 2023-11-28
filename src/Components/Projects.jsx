f226c18a3299b5a4fe57bed17a2bb57388130cec
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2023-11-28 19:07:15 +0530

    Update Files

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { CardLink, Image } from 'react-bootstrap';
 import './Home.css';
